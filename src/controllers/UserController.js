a5e4e13e0f497ad87936f414d5d923932be73a16
Rilton Franzone <riltonfranzone@gmail.com>
2020-05-01 14:07:04 -0300

    adding users CRUD

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,7 +3,41 @@ const knex = require('../database');
 module.exports = {
   async index(req, res) {
     const results = await knex('users');
-  
-    return res.json(results)
+    return res.json(results);
+  },
+
+  async create(req, res, next) {
+    const { username } = req.body;
+
+    try {
+      await knex('users').insert({ username });
+      return res.status(201).send();
+
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  async update(req, res, next) {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    try {
+      await knex('users').where('id', id ).update({username});
+      return res.send();
+    } catch (err) {
+      next(err)
   }
+},
+
+  async delete(req, res, next) {
+  const { id } = req.params;
+
+  try {
+    await knex('users').where('id', id ).del();
+    return res.send();
+  } catch (err) {
+    next(err)
+  }
+}
 }
--- src/server.js
+++ src/server.js
@@ -3,6 +3,19 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(express.json())
 app.use(routes);
+
+app.use((err, req, res, next) => {
+  const error = new Error('Not found');
+  err.status = 404;
+  next(err)
+})
+
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.json({err: err.message})
+})
 
 app.listen(3333, () => console.log('Server running on port 3333'));
